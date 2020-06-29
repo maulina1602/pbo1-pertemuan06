ba23f78de6f8531ebe5a3e4f07362a60e5f317a0
maulina1602 <linamaulina.ml@gmail.com>
2020-06-30 05:04:21 +0800

    Overriding

---
+++ src/Bumi.java
@@ -0,0 +1,15 @@
+public class Bumi extends Planet {
+    public void namaPlanet(){
+
+    }
+
+    @Override
+    void namaPlanet(String karakter) {
+        super.namaPlanet(karakter);
+    }
+
+    @Override
+    void namaPlanet(String nama, String warna, int rotasi, int revolusi, double diameter) {
+        super.namaPlanet(nama, warna, rotasi, revolusi, diameter);
+    }
+}
